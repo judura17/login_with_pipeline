83560d804d1090dc5265fc04d31c08589659e0d6
unknown <judura17@gmail.com>
2026-07-23 18:11:49 -0500

    Configuracion de Chrome para Jenkins v2

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -7,14 +7,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utils.SharedContext;
 
 public class Hooks {
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        SharedContext.driver = new ChromeDriver();
-        SharedContext.driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        SharedContext.driver = new ChromeDriver(options);
+
+        //SharedContext.driver.manage().window().maximize();
         SharedContext.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
     }
